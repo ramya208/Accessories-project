573049d3ff8d8a489ac6b7b30a980991287163aa
ramya208 <tramya19092006@gmail.com>
2026-09-25 14:11:27 +0530

    Fix admin product visibility

--- products/views.py
+++ products/views.py
@@ -27,16 +27,24 @@ from rest_framework_simplejwt.tokens import RefreshToken
 #     return render(request, 'home.html', {
 #         'products': products
 #     })
+# def product_list(request):
+
+#       products = Product.objects.filter(
+#         owner=request.user
+#       )
+
+#     return render(request, "products.html", {
+#         "products": products
+#     })
 def product_list(request):
 
-    # products = Product.objects.all()
-     products = Product.objects.filter(
+    products = Product.objects.filter(
         owner=request.user
+    )
 
     return render(request, "products.html", {
         "products": products
     })
-
 
 # def signup_view(request):
 
